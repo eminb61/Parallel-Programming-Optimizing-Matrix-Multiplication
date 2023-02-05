953650bbacc1ea3db8e46d345c59b47be94d2c17
Ziyang Wu <robinwu@login06.chn>
2023-02-05 15:52:34 -0800

    40 percent done

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -3,10 +3,13 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 // #define BLOCK_SIZE 64
 // #define BLOCK_SIZE_L1 64
 // #define BLOCK_SIZE_L2 256
-#define BLK_J 128
-#define BLK_K 64
-#define BLK_I 32
+#define BLK_J 512
+#define BLK_K 128
+#define BLK_I 64
 
+#define KS 8
+
+#define ALIGNMENT 64
 #define min(a, b) (((a) < (b)) ? (a) : (b))
 
 // #define alpha( i,j ) A[ (j)*lda + (i) ]   // map alpha( i,j ) to array A
@@ -49,184 +52,129 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 //     // printf("Address of iptr: %p\n", (void*)&iptr);
 // }
 
-static void kernel4by4_packed(int lda, int K, double *MP_A, double *MP_B, double *C)
+// static void kernel4by4_packed(int lda, int K, double *MP_A, double *MP_B, double *C)
+// {
+//   __m256d gamma_0123_0 = _mm256_loadu_pd( &CC( 0,0 ) );
+//   __m256d gamma_0123_1 = _mm256_loadu_pd( &CC( 0,1 ) );
+//   __m256d gamma_0123_2 = _mm256_loadu_pd( &CC( 0,2 ) );
+//   __m256d gamma_0123_3 = _mm256_loadu_pd( &CC( 0,3 ) );
+
+//   __m256d beta_p_j;
+   	
+//   for ( int p=0; p<K; p++ ){
+//     /* load alpha( 0:3, p ) */
+//     __m256d alpha_0123_p = _mm256_loadu_pd( MP_A );
+
+//     /* load beta( p, 0 ); update gamma( 0:3, 0 ) */
+//     beta_p_j = _mm256_broadcast_sd( MP_B );
+//     gamma_0123_0 = _mm256_fmadd_pd( alpha_0123_p, beta_p_j, gamma_0123_0 );
+
+//     /* load beta( p, 1 ); update gamma( 0:3, 1 ) */
+//     beta_p_j = _mm256_broadcast_sd( MP_B+1 );
+//     gamma_0123_1 = _mm256_fmadd_pd( alpha_0123_p, beta_p_j, gamma_0123_1 );
+
+//     /* load beta( p, 2 ); update gamma( 0:3, 2 ) */
+//     beta_p_j = _mm256_broadcast_sd( MP_B+2 );
+//     gamma_0123_2 = _mm256_fmadd_pd( alpha_0123_p, beta_p_j, gamma_0123_2 );
+
+//     /* load beta( p, 3 ); update gamma( 0:3, 3 ) */
+//     beta_p_j = _mm256_broadcast_sd( MP_B+3 );
+//     gamma_0123_3 = _mm256_fmadd_pd( alpha_0123_p, beta_p_j, gamma_0123_3 );
+
+//     MP_A += 4;
+//     MP_B += 4;
+//   }
+
+//   /* Store the updated results.  This should be done more carefully since
+//      there may be an incomplete micro-tile. */
+//   _mm256_storeu_pd( &CC(0,0), gamma_0123_0 );
+//   _mm256_storeu_pd( &CC(0,1), gamma_0123_1 );
+//   _mm256_storeu_pd( &CC(0,2), gamma_0123_2 );
+//   _mm256_storeu_pd( &CC(0,3), gamma_0123_3 );
+// }
+
+static void kernel8by8_packed(int lda, int K, double *MP_A, double *MP_B, double *C)
 {
-  __m256d gamma_0123_0 = _mm256_loadu_pd( &CC( 0,0 ) );
-  __m256d gamma_0123_1 = _mm256_loadu_pd( &CC( 0,1 ) );
-  __m256d gamma_0123_2 = _mm256_loadu_pd( &CC( 0,2 ) );
-  __m256d gamma_0123_3 = _mm256_loadu_pd( &CC( 0,3 ) );
+  __m256d gamma_0_0 = _mm256_loadu_pd( &CC( 0,0 ) );
+  __m256d gamma_0_1 = _mm256_loadu_pd( &CC( 4,0 ) );
+  __m256d gamma_1_0 = _mm256_loadu_pd( &CC( 0,1 ) );
+  __m256d gamma_1_1 = _mm256_loadu_pd( &CC( 4,1 ) );
+  __m256d gamma_2_0 = _mm256_loadu_pd( &CC( 0,2 ) );
+  __m256d gamma_2_1 = _mm256_loadu_pd( &CC( 4,2 ) );
+  __m256d gamma_3_0 = _mm256_loadu_pd( &CC( 0,3 ) );
+  __m256d gamma_3_1 = _mm256_loadu_pd( &CC( 4,3 ) );
+  __m256d gamma_4_0 = _mm256_loadu_pd( &CC( 0,4 ) );
+  __m256d gamma_4_1 = _mm256_loadu_pd( &CC( 4,4 ) );
+  __m256d gamma_5_0 = _mm256_loadu_pd( &CC( 0,5 ) );
+  __m256d gamma_5_1 = _mm256_loadu_pd( &CC( 4,5 ) );
+  __m256d gamma_6_0 = _mm256_loadu_pd( &CC( 0,6 ) );
+  __m256d gamma_6_1 = _mm256_loadu_pd( &CC( 4,6 ) );
+  __m256d gamma_7_0 = _mm256_loadu_pd( &CC( 0,7 ) );
+  __m256d gamma_7_1 = _mm256_loadu_pd( &CC( 4,7 ) );
+
 
   __m256d beta_p_j;
    	
   for ( int p=0; p<K; p++ ){
-    /* load alpha( 0:3, p ) */
-    __m256d alpha_0123_p = _mm256_loadu_pd( MP_A );
+    
+    __m256d alpha_p_0 = _mm256_loadu_pd( MP_A );
+    __m256d alpha_p_1 = _mm256_loadu_pd( MP_A + 4 );
 
-    /* load beta( p, 0 ); update gamma( 0:3, 0 ) */
     beta_p_j = _mm256_broadcast_sd( MP_B );
-    gamma_0123_0 = _mm256_fmadd_pd( alpha_0123_p, beta_p_j, gamma_0123_0 );
+    gamma_0_0 = _mm256_fmadd_pd( alpha_p_0, beta_p_j, gamma_0_0 );
+    gamma_0_1 = _mm256_fmadd_pd( alpha_p_1, beta_p_j, gamma_0_1 );
 
-    /* load beta( p, 1 ); update gamma( 0:3, 1 ) */
     beta_p_j = _mm256_broadcast_sd( MP_B+1 );
-    gamma_0123_1 = _mm256_fmadd_pd( alpha_0123_p, beta_p_j, gamma_0123_1 );
+    gamma_1_0 = _mm256_fmadd_pd( alpha_p_0, beta_p_j, gamma_1_0 );
+    gamma_1_1 = _mm256_fmadd_pd( alpha_p_1, beta_p_j, gamma_1_1 );
 
-    /* load beta( p, 2 ); update gamma( 0:3, 2 ) */
     beta_p_j = _mm256_broadcast_sd( MP_B+2 );
-    gamma_0123_2 = _mm256_fmadd_pd( alpha_0123_p, beta_p_j, gamma_0123_2 );
+    gamma_2_0 = _mm256_fmadd_pd( alpha_p_0, beta_p_j, gamma_2_0 );
+    gamma_2_1 = _mm256_fmadd_pd( alpha_p_1, beta_p_j, gamma_2_1 );
 
-    /* load beta( p, 3 ); update gamma( 0:3, 3 ) */
     beta_p_j = _mm256_broadcast_sd( MP_B+3 );
-    gamma_0123_3 = _mm256_fmadd_pd( alpha_0123_p, beta_p_j, gamma_0123_3 );
+    gamma_3_0 = _mm256_fmadd_pd( alpha_p_0, beta_p_j, gamma_3_0 );
+    gamma_3_1 = _mm256_fmadd_pd( alpha_p_1, beta_p_j, gamma_3_1 );
 
-    MP_A += 4;
-    MP_B += 4;
+    beta_p_j = _mm256_broadcast_sd( MP_B+4 );
+    gamma_4_0 = _mm256_fmadd_pd( alpha_p_0, beta_p_j, gamma_4_0 );
+    gamma_4_1 = _mm256_fmadd_pd( alpha_p_1, beta_p_j, gamma_4_1 );
+
+    beta_p_j = _mm256_broadcast_sd( MP_B+5 );
+    gamma_5_0 = _mm256_fmadd_pd( alpha_p_0, beta_p_j, gamma_5_0 );
+    gamma_5_1 = _mm256_fmadd_pd( alpha_p_1, beta_p_j, gamma_5_1 );
+
+    beta_p_j = _mm256_broadcast_sd( MP_B+6 );
+    gamma_6_0 = _mm256_fmadd_pd( alpha_p_0, beta_p_j, gamma_6_0 );
+    gamma_6_1 = _mm256_fmadd_pd( alpha_p_1, beta_p_j, gamma_6_1 );
+
+    beta_p_j = _mm256_broadcast_sd( MP_B+7 );
+    gamma_7_0 = _mm256_fmadd_pd( alpha_p_0, beta_p_j, gamma_7_0 );
+    gamma_7_1 = _mm256_fmadd_pd( alpha_p_1, beta_p_j, gamma_7_1 );
+
+    MP_A += 8;
+    MP_B += 8;
   }
 
   /* Store the updated results.  This should be done more carefully since
      there may be an incomplete micro-tile. */
-  _mm256_storeu_pd( &CC(0,0), gamma_0123_0 );
-  _mm256_storeu_pd( &CC(0,1), gamma_0123_1 );
-  _mm256_storeu_pd( &CC(0,2), gamma_0123_2 );
-  _mm256_storeu_pd( &CC(0,3), gamma_0123_3 );
+  _mm256_storeu_pd( &CC(0,0), gamma_0_0 );
+  _mm256_storeu_pd( &CC(4,0), gamma_0_1 );
+  _mm256_storeu_pd( &CC(0,1), gamma_1_0 );
+  _mm256_storeu_pd( &CC(4,1), gamma_1_1 );
+  _mm256_storeu_pd( &CC(0,2), gamma_2_0 );
+  _mm256_storeu_pd( &CC(4,2), gamma_2_1 );
+  _mm256_storeu_pd( &CC(0,3), gamma_3_0 );
+  _mm256_storeu_pd( &CC(4,3), gamma_3_1 );
+  _mm256_storeu_pd( &CC(0,4), gamma_4_0 );
+  _mm256_storeu_pd( &CC(4,4), gamma_4_1 );
+  _mm256_storeu_pd( &CC(0,5), gamma_5_0 );
+  _mm256_storeu_pd( &CC(4,5), gamma_5_1 );
+  _mm256_storeu_pd( &CC(0,6), gamma_6_0 );
+  _mm256_storeu_pd( &CC(4,6), gamma_6_1 );
+  _mm256_storeu_pd( &CC(0,7), gamma_7_0 );
+  _mm256_storeu_pd( &CC(4,7), gamma_7_1 );
 }
-
-// static inline void kernel4by4(int lda, int K, double *A, double *B,double *C) {
-//     /* Declare vector registers to hold 4x4 C and load them */
-//     __m256d gamma_0123_0 = _mm256_loadu_pd( &CC( 0,0 ) );
-//     __m256d gamma_0123_1 = _mm256_loadu_pd( &CC( 0,1 ) );
-//     __m256d gamma_0123_2 = _mm256_loadu_pd( &CC( 0,2 ) );
-//     __m256d gamma_0123_3 = _mm256_loadu_pd( &CC( 0,3 ) );
-
-//     for ( int p=0; p<K; p++ ){
-//     /* Declare vector register for load/broadcasting BB( p,j ) */
-//     __m256d beta_p_j;
-
-//     /* Declare a vector register to hold the current column of A and load
-//         it with the four elements of that column. */
-//     __m256d alpha_0123_p = _mm256_loadu_pd( &AA( 0,p ) );
-
-//     /* Load/broadcast beta( p,0 ). */
-//     beta_p_j = _mm256_broadcast_sd( &BB( p, 0) );
-
-//     /* update the first column of C with the current column of A times
-//         beta ( p,0 ) */
-//     gamma_0123_0 = _mm256_fmadd_pd( alpha_0123_p, beta_p_j, gamma_0123_0 );
-
-//     /* REPEAT for second, third, and fourth columns of C.  Notice that the 
-//         current column of A needs not be reloaded. */
-//     beta_p_j = _mm256_broadcast_sd( &BB( p, 1) );
-//     gamma_0123_1 = _mm256_fmadd_pd( alpha_0123_p, beta_p_j, gamma_0123_1 );
-
-//     beta_p_j = _mm256_broadcast_sd( &BB( p, 2) );
-//     gamma_0123_2 = _mm256_fmadd_pd( alpha_0123_p, beta_p_j, gamma_0123_2 );
-
-//     beta_p_j = _mm256_broadcast_sd( &BB( p, 3) );
-//     gamma_0123_3 = _mm256_fmadd_pd( alpha_0123_p, beta_p_j, gamma_0123_3 );
-//     }
-
-//     /* Store the updated results */
-//     _mm256_storeu_pd( &CC(0,0), gamma_0123_0 );
-//     _mm256_storeu_pd( &CC(0,1), gamma_0123_1 );
-//     _mm256_storeu_pd( &CC(0,2), gamma_0123_2 );
-//     _mm256_storeu_pd( &CC(0,3), gamma_0123_3 );
-// }
-
-// static inline void kernel4by4(int lda, int K, double* A, double* B, double* C) {
-//     double c00, c01, c02, c03, c10, c11, c12, c13, c20, c21, c22, c23, c30, c31, c32, c33;
-//     c00 = C[0];
-//     c10 = C[1];
-//     c20 = C[2];
-//     c30 = C[3];
-//     c01 = C[lda];
-//     c11 = C[1 + lda];
-//     c21 = C[2 + lda];
-//     c31 = C[3 + lda];
-//     c02 = C[2 * lda];
-//     c12 = C[1 + 2 * lda];
-//     c22 = C[2 + 2 * lda];
-//     c32 = C[3 + 2 * lda];
-//     c03 = C[3 * lda];
-//     c13 = C[1 + 3 * lda];
-//     c23 = C[2 + 3 * lda];
-//     c33 = C[3 + 3 * lda];
-//     for (int k = 0; k < K; k++) {
-//         double a0x, a1x, a2x, a3x, bx0, bx1, bx2, bx3;
-//         a0x = A[k * lda];
-//         a1x = A[1 + k * lda];
-//         a2x = A[2 + k * lda];
-//         a3x = A[3 + k * lda];
-//         bx0 = B[k];
-//         bx1 = B[k + lda];
-//         bx2 = B[k + 2 * lda];
-//         bx3 = B[k + 3 * lda];
-        
-//         c00 += a0x * bx0;
-//         c01 += a0x * bx1;
-//         c02 += a0x * bx2;
-//         c03 += a0x * bx3;
-//         c10 += a1x * bx0;
-//         c11 += a1x * bx1;
-//         c12 += a1x * bx2;
-//         c13 += a1x * bx3;
-//         c20 += a2x * bx0;
-//         c21 += a2x * bx1;
-//         c22 += a2x * bx2;
-//         c23 += a2x * bx3;
-//         c30 += a3x * bx0;
-//         c31 += a3x * bx1;
-//         c32 += a3x * bx2;
-//         c33 += a3x * bx3;
-//     }
-//     C[0] = c00;
-//     C[1] = c10;
-//     C[2] = c20;
-//     C[3] = c30;
-//     C[lda] = c01;
-//     C[1 + lda] = c11;
-//     C[2 + lda] = c21;
-//     C[3 + lda] = c31;
-//     C[2 * lda] = c02;
-//     C[1 + 2 * lda] = c12;
-//     C[2 + 2 * lda] = c22;
-//     C[3 + 2 * lda] = c32;
-//     C[3 * lda] = c03;
-//     C[1 + 3 * lda] = c13;
-//     C[2 + 3 * lda] = c23;
-//     C[3 + 3 * lda] = c33;
-// }
-
-
-// /* This routine performs a dgemm operation
-//  *  C := C + A * B
-//  * where A, B, and C are lda-by-lda matrices stored in column-major format.
-//  * On exit, A and B maintain their input values. */
-// void square_dgemm(int lda, double* A, double* B, double* C) {
-//     // For each block-row of A
-//     for (int v = 0; v < lda; v += BLOCK_SIZE_L2) {
-//         int lda_j = v + min(BLOCK_SIZE_L2, lda - v);
-//         for (int w = 0; w < lda; w += BLOCK_SIZE_L2) {
-//             int lda_k = w + min(BLOCK_SIZE_L2, lda - w);
-//             for (int u = 0; u < lda; u += BLOCK_SIZE_L2) {
-//                 // Correct block dimensions if block "goes off edge of" the matrix
-//                 int lda_i = u + min(BLOCK_SIZE_L2, lda - u);
-//                 for (int j = v; j < lda_j; j += BLOCK_SIZE_L1) {
-//                     int N = min(BLOCK_SIZE_L1, lda_j - j);
-//                     // For each block-column of B
-//                     for (int k = w; k < lda_k; k += BLOCK_SIZE_L1) {
-//                         int K = min(BLOCK_SIZE_L1, lda_k - k);
-//                         // Accumulate block dgemms into block of C
-//                         for (int i = u; i < lda_i; i += BLOCK_SIZE_L1) {
-//                             // Correct block dimensions if block "goes off edge of" the matrix
-//                             int M = min(BLOCK_SIZE_L1, lda_i - i);
-//                             // Perform individual block dgemm
-//                             do_block(lda, M, N, K, A + i + k * lda, B + k + j * lda, C + i + j * lda);
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// }
 
 // static inline void copy_a (int lda, int K, double* a_src, double* a_dest) {
 //   /* For each 4xK block-row of A */
@@ -322,14 +270,17 @@ static void kernel4by4_packed(int lda, int K, double *MP_A, double *MP_B, double
 // }
 
 static inline void copy_mem (int K, double* a_src, double* a_dest) {
-  /* For each 4xK block-row of A */
   for (int i = 0; i < K; ++i) 
-  {
-    *a_dest++ = *a_src;
-    *a_dest++ = *(a_src+1);
-    *a_dest++ = *(a_src+2);
-    *a_dest++ = *(a_src+3);
-    a_src += 4;
+  { 
+    /* Unroll */
+    // *a_dest++ = *a_src;
+    // *a_dest++ = *(a_src+1);
+    // *a_dest++ = *(a_src+2);
+    // *a_dest++ = *(a_src+3);
+    // a_src += 4;
+    for (int j = 0; j < KS; j++){
+        *a_dest++ = *a_src++;
+    }
   }
 }
 
@@ -339,10 +290,14 @@ static inline void PackMicroPanelA_MRxKC(int K, int lda, double *A, double *Atil
   /* March through A in column-major order, packing into Atilde as we go. */
     /* Full row size micro-panel.*/
     for (int p=0; p<K; p++){
-        *Atilde++ = AA(0, p);
-        *Atilde++ = AA(1, p);
-        *Atilde++ = AA(2, p);
-        *Atilde++ = AA(3, p);
+        /*unroll this*/
+        // *Atilde++ = AA(0, p);
+        // *Atilde++ = AA(1, p);
+        // *Atilde++ = AA(2, p);
+        // *Atilde++ = AA(3, p);
+        for ( int i=0; i<KS; i++ ){
+            *Atilde++ = AA(i, p);
+        }  
     }
 }
 
@@ -351,7 +306,7 @@ static inline void PackMicroPanelA_MRxKC_Pad(int K, int Medge, int lda, double *
    This is an unoptimized implementation for general MR and KC. */
   /* March through A in column-major order, packing into Atilde as we go. */
     /* Full row size micro-panel.*/
-    int Mpad = 4 - Medge;
+    int Mpad = KS - Medge;
     for (int p=0; p<K; p++){
         for (int i=0; i<Medge; i++) {
             *Atilde++ = AA(i, p);
@@ -367,11 +322,11 @@ static inline void PackBlockA_MCxKC(int M, int K, int lda, double *A, double *At
     be a multiple of MR.  The block is packed into Atilde a micro-panel
     at a time. If necessary, the last micro-panel is padded with rows
     of zeroes. */
-    int Medge = M % 4;
+    int Medge = M % KS;
     int Mmax = M - Medge;
-    for (int i=0; i<Mmax; i+= 4){
+    for (int i=0; i<Mmax; i+= KS){
         PackMicroPanelA_MRxKC(K, lda, &AA(i, 0), Atilde);
-        Atilde += K * 4;
+        Atilde += K * KS;
     }
     if (Medge != 0){
         PackMicroPanelA_MRxKC_Pad(K, Medge, lda, &AA(Mmax, 0), Atilde);
@@ -383,7 +338,7 @@ static inline void PackMicroPanelB_KCxNR_Pad(int K, int Nedge, int lda, double *
    This is an unoptimized implementation for general KC and NR.
    k is assumed to be less then or equal to KC.
    n is assumed to be less then or equal to NR.  */
-    int Npad = 4 - Nedge;
+    int Npad = KS - Nedge;
     for (int p=0; p<K; p++){
         for (int j=0; j<Nedge; j++){
             *Btilde++ = BB(p, j);
@@ -400,34 +355,40 @@ static inline void PackMicroPanelB_KCxNR(int K, int lda, double *B, double *Btil
    k is assumed to be less then or equal to KC.
    n is assumed to be less then or equal to NR.  */
     for (int p=0; p<K; p++){
-        *Btilde++ = BB(p, 0);
-        *Btilde++ = BB(p, 1);
-        *Btilde++ = BB(p, 2);
-        *Btilde++ = BB(p, 3);
+        // *Btilde++ = BB(p, 0);
+        // *Btilde++ = BB(p, 1);
+        // *Btilde++ = BB(p, 2);
+        // *Btilde++ = BB(p, 3);
+        for ( int j=0; j<KS; j++ ){
+            *Btilde++ = BB(p, j);
+        }  
     }
 }
 
 static inline void PackPanelB_KCxNC(int K, int N, int lda, double *B, double *Btilde){
 /* Pack a k x n panel of B in to a KC x NC buffer.
    The block is copied into Btilde a micro-panel at a time. */
-    int Nedge = N % 4;
+    int Nedge = N % KS;
     int Nmax = N - Nedge;
-    for (int j=0; j<Nmax; j+= 4){
+    for (int j=0; j<Nmax; j+= KS){
         PackMicroPanelB_KCxNR(K, lda, &BB(0, j), Btilde);
-        Btilde += K * 4;
+        Btilde += K * KS;
     }
     if (Nedge != 0){
         PackMicroPanelB_KCxNR_Pad(K, Nedge, lda, &BB(0, Nmax), Btilde);
     }
    
 }
+static double * restrict Ctilde __attribute__ ((aligned(ALIGNMENT)));
+static double * restrict Atilde __attribute__ ((aligned(ALIGNMENT)));
+static double * restrict Btilde __attribute__ ((aligned(ALIGNMENT)));
 
 void square_dgemm(int lda, double* A, double* B, double* C) {
     // For each block-row of A
     double *c_p;
-    int blk = (lda + 4 - 1) / 4;
-    int ldc = blk * 4;
-    double *Ctilde = (double *) malloc(ldc * ldc * sizeof(double)); 
+    int blk = (lda + KS- 1) / KS;
+    int ldc = blk * KS;
+    Ctilde = _mm_malloc(ldc * ldc * sizeof(double), ALIGNMENT);
     if (ldc != lda) {
         for (int i = 0; i < lda; i ++){
             memcpy(Ctilde + ldc * i, C + lda * i, lda * sizeof(double));
@@ -435,40 +396,40 @@ void square_dgemm(int lda, double* A, double* B, double* C) {
     }else{
         memcpy(Ctilde, C, lda * lda * sizeof(double));
     }
+    // double *Btilde = (double *) malloc(BLK_K * BLK_J * sizeof(double)); // Target L3, KC * NC
+    Btilde = _mm_malloc(BLK_K * BLK_J * sizeof(double), ALIGNMENT);
+    // double *Atilde = (double *) malloc(BLK_I * BLK_K * sizeof(double)); // Target L2, MC * KC
+    Atilde = _mm_malloc(BLK_I * BLK_K * sizeof(double), ALIGNMENT);
     for (int j = 0; j < lda; j += BLK_J) {
         // For each block-column of B
         int N = min(BLK_J, lda - j);
         
-        double *Btilde = (double *) malloc(BLK_K * BLK_J * sizeof(double)); // Target L3, KC * NC
+        
         for (int k = 0; k < lda; k += BLK_K) {
             // Accumulate block dgemms into block of C
             int K = min(BLK_K, lda - k);
             
             PackPanelB_KCxNC(K, N, lda, &BB(k, j), Btilde);
-            
-            double *Atilde = (double *) malloc(BLK_I * BLK_K * sizeof(double)); // Target L2, MC * KC
-            
             for (int i = 0; i < lda; i += BLK_I) {
                 // Correct block dimensions if block "goes off edge of" the matrix
                 int M = min(BLK_I, lda - i);
                 PackBlockA_MCxKC(M, K, lda, &AA(i, k), Atilde);
                 // Perform individual block dgemm
                 c_p = &Ctilde[i + j*ldc];
-                double B_block[4*K];
+                double B_block[KS*K];
                 double *b_ptr;
-                for (int v = 0; v < N; v+=4) {
+                for (int v = 0; v < N; v+=KS) {
                     b_ptr = &B_block[0];
                     copy_mem(K, &Btilde[v*K], b_ptr);
-                    for (int u = 0; u < M; u+=4) {
+                    for (int u = 0; u < M; u+=KS) {
                         // a_ptr = &A_block[0];
                         // if (v == 0) copy_mem(K, &Atilde[u*K], a_ptr);
-                        kernel4by4_packed(ldc, K, &Atilde[u*K], b_ptr, &c_p[u + v*ldc]);
+                        // kernel4by4_packed(ldc, K, &Atilde[u*K], b_ptr, &c_p[u + v*ldc]);
+                        kernel8by8_packed(ldc, K, &Atilde[u*K], b_ptr, &c_p[u + v*ldc]);
                     }
                 }
             }
-            free(Atilde);
         }
-        free(Btilde);
     }
     if (ldc != lda) {
         for (int i = 0; i < lda; i ++){
@@ -477,4 +438,6 @@ void square_dgemm(int lda, double* A, double* B, double* C) {
     }else{
         memcpy(C, Ctilde, lda * lda * sizeof(double));
     }
+    // free(Atilde);
+    // free(Btilde);
 }
