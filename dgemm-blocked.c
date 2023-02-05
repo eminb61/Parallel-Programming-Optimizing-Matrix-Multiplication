e09fda86a74bc2ce12673b5c14c0bf85095c2884
shouc <shou@ucsb.edu>
2023-02-04 17:33:09 -0800

    avx

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -1,4 +1,10 @@
 const char* dgemm_desc = "Simple blocked dgemm.";
+#include <mmintrin.h>
+#include <immintrin.h>
+#include <xmmintrin.h>
+#include <pmmintrin.h>
+#include <emmintrin.h>
+#include <string.h>
 
 #define BLOCK_SIZE 64
 #define BLOCK_SIZE_L1 64
@@ -7,70 +13,94 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 #define min(a, b) (((a) < (b)) ? (a) : (b))
 
 
-
 static inline void kernel4by4(int lda, int K, double* A, double* B, double* C) {
-    double c00, c01, c02, c03, c10, c11, c12, c13, c20, c21, c22, c23, c30, c31, c32, c33;
-    c00 = C[0];
-    c10 = C[1];
-    c20 = C[2];
-    c30 = C[3];
-    c01 = C[lda];
-    c11 = C[1 + lda];
-    c21 = C[2 + lda];
-    c31 = C[3 + lda];
-    c02 = C[2 * lda];
-    c12 = C[1 + 2 * lda];
-    c22 = C[2 + 2 * lda];
-    c32 = C[3 + 2 * lda];
-    c03 = C[3 * lda];
-    c13 = C[1 + 3 * lda];
-    c23 = C[2 + 3 * lda];
-    c33 = C[3 + 3 * lda];
+    // double c00, c01, c02, c03, c10, c11, c12, c13, c20, c21, c22, c23, c30, c31, c32, c33;
+    // c00 = C[0];
+    // c10 = C[1];
+    // c20 = C[2];
+    // c30 = C[3];
+
+    __m256d C0 = _mm256_loadu_pd(C);
+    __m256d C1 = _mm256_loadu_pd(C + lda);
+    __m256d C2 = _mm256_loadu_pd(C + 2 * lda);
+    __m256d C3 = _mm256_loadu_pd(C + 3 * lda);
+
+    // c01 = C[lda];
+    // c11 = C[1 + lda];
+    // c21 = C[2 + lda];
+    // c31 = C[3 + lda];
+    // c02 = C[2 * lda];
+    // c12 = C[1 + 2 * lda];
+    // c22 = C[2 + 2 * lda];
+    // c32 = C[3 + 2 * lda];
+    // c03 = C[3 * lda];
+    // c13 = C[1 + 3 * lda];
+    // c23 = C[2 + 3 * lda];
+    // c33 = C[3 + 3 * lda];
     for (int k = 0; k < K; k++) {
-        double a0x, a1x, a2x, a3x, bx0, bx1, bx2, bx3;
-        a0x = A[k * lda];
-        a1x = A[1 + k * lda];
-        a2x = A[2 + k * lda];
-        a3x = A[3 + k * lda];
-        bx0 = B[k];
-        bx1 = B[k + lda];
-        bx2 = B[k + 2 * lda];
-        bx3 = B[k + 3 * lda];
-        
-        c00 += a0x * bx0;
-        c01 += a0x * bx1;
-        c02 += a0x * bx2;
-        c03 += a0x * bx3;
-        c10 += a1x * bx0;
-        c11 += a1x * bx1;
-        c12 += a1x * bx2;
-        c13 += a1x * bx3;
-        c20 += a2x * bx0;
-        c21 += a2x * bx1;
-        c22 += a2x * bx2;
-        c23 += a2x * bx3;
-        c30 += a3x * bx0;
-        c31 += a3x * bx1;
-        c32 += a3x * bx2;
-        c33 += a3x * bx3;
+        // double a0x, a1x, a2x, a3x, bx0, bx1, bx2, bx3;
+
+        __m256d ax = _mm256_loadu_pd(A + k * lda);
+        __m256d bx0 = _mm256_broadcast_sd(B + k);
+        __m256d bx1 = _mm256_broadcast_sd(B + k + lda);
+        __m256d bx2 = _mm256_broadcast_sd(B + k + 2 * lda);
+        __m256d bx3 = _mm256_broadcast_sd(B + k + 3 * lda);
+        // a0x = A[k * lda];
+        // a1x = A[1 + k * lda];
+        // a2x = A[2 + k * lda];
+        // a3x = A[3 + k * lda];
+        // bx0 = B[k];
+        // bx1 = B[k + lda];
+        // bx2 = B[k + 2 * lda];
+        // bx3 = B[k + 3 * lda];
+
+
+        C0 = _mm256_add_pd(C0, _mm256_mul_pd(ax,bx0));
+        C1 = _mm256_add_pd(C1, _mm256_mul_pd(ax,bx1));
+        C2 = _mm256_add_pd(C2, _mm256_mul_pd(ax,bx2));
+        C3 = _mm256_add_pd(C3, _mm256_mul_pd(ax,bx3));
+        // c00 += a0x * bx0;
+        // c01 += a0x * bx1;
+        // c02 += a0x * bx2;
+        // c03 += a0x * bx3;
+        // c10 += a1x * bx0;
+        // c11 += a1x * bx1;
+        // c12 += a1x * bx2;
+        // c13 += a1x * bx3;
+        // c20 += a2x * bx0;
+        // c21 += a2x * bx1;
+        // c22 += a2x * bx2;
+        // c23 += a2x * bx3;
+        // c30 += a3x * bx0;
+        // c31 += a3x * bx1;
+        // c32 += a3x * bx2;
+        // c33 += a3x * bx3;
     }
-    C[0] = c00;
-    C[1] = c10;
-    C[2] = c20;
-    C[3] = c30;
-    C[lda] = c01;
-    C[1 + lda] = c11;
-    C[2 + lda] = c21;
-    C[3 + lda] = c31;
-    C[2 * lda] = c02;
-    C[1 + 2 * lda] = c12;
-    C[2 + 2 * lda] = c22;
-    C[3 + 2 * lda] = c32;
-    C[3 * lda] = c03;
-    C[1 + 3 * lda] = c13;
-    C[2 + 3 * lda] = c23;
-    C[3 + 3 * lda] = c33;
+
+
+    _mm256_storeu_pd(C,C0);
+    _mm256_storeu_pd(C + lda,C1);
+    _mm256_storeu_pd(C + 2*lda,C2);
+    _mm256_storeu_pd(C+3*lda,C3);
+    // C[0] = c00;
+    // C[1] = c10;
+    // C[2] = c20;
+    // C[3] = c30;
+    // C[lda] = c01;
+    // C[1 + lda] = c11;
+    // C[2 + lda] = c21;
+    // C[3 + lda] = c31;
+    // C[2 * lda] = c02;
+    // C[1 + 2 * lda] = c12;
+    // C[2 + 2 * lda] = c22;
+    // C[3 + 2 * lda] = c32;
+    // C[3 * lda] = c03;
+    // C[1 + 3 * lda] = c13;
+    // C[2 + 3 * lda] = c23;
+    // C[3 + 3 * lda] = c33;
 }
+
+
 
 static inline void do_block(int lda, int M, int N, int K, double* A, double* B, double* C) {
     // For each row i of A
